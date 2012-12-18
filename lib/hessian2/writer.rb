1b0a9fe78e9953eba93264cc70825ee9734d338c
takafan <takafan@163.com>
2012-12-18 21:32:50 +0800

    a

--- lib/hessian2/writer.rb
+++ lib/hessian2/writer.rb
@@ -1,72 +1,50 @@
+# com.caucho.hessian.io Hessian2Output
+require 'hessian2/constants'
+
 module Hessian2
   module Writer
-    CHUNK_SIZE = 32768
 
     def call(method, args)
       refs = {}
       out = [ 'H', '2', '0', 'C' ].pack('ahha')
-      out << [method.size].pack('n') << method
-      out << [args.size].pack('n')
-      args.each { |arg| out << write_object(arg, refs) }
+      out << [method.size].pack('C') << method
+      out << [args.size].pack('C')
+      args.each { |arg| out << write_val(arg, refs) }
       out
     end
 
     def reply(val)
       out = [ 'H', '2', '0', 'R' ].pack('ahha')
-      out << write_object(val)
+      out << write_val(val)
       out
     end
 
-    def fault(e)
+    def fault(e)  # TODO
       clbl, mlbl, dlbl = 'code', 'message', 'detail'
       code, message, detail = e.class.to_s, e.message, e.backtrace
       out = [ 'F' ].pack('a')
       out << [ 'H' ].pack('a')
       out << [ clbl.size ].pack('n') << clbl
-      out << [ code.size ].pack('n') << write_object(code)
+      out << [ code.size ].pack('n') << write_val(code)
       out << [ mlbl.size ].pack('n') << mlbl
-      out << [ message.size ] write_object(message)
+      out << [ message.size ] write_val(message)
       out << [ dlbl.size ].pack('n') << dlbl
-      out << write_object(backtrace)
+      out << write_val(backtrace)
       out << 'Z'
     end
 
     private
-    def write_object(val, refs = {}, chunks = [], type = nil)
+    def write_val(val, refs = {}, type = nil)
       case val
       when TypeWrapper # TODO
         obj, hessian_type = val.object, val.hessian_type
         case hessian_type
         when 'L', 'Long', 'long'  # declare fixnum as long
           [ 'L', obj ].pack('aq>')  # long
-        when 'X', 'x' # declare string as xml
-          if obj.size > CHUNK_SIZE
-            chunk = obj.slice!(0, CHUNK_SIZE)
-            if chunk.ascii_only?
-              chunks << [ 's', CHUNK_SIZE ].pack('an') << chunk
-            else
-              chunks << [ 's', CHUNK_SIZE, chunk.unpack('U*') ].flatten.pack('anU*')
-            end
-            write_object(TypeWrapper.new('X', obj), refs, chunks)
-          else
-            if obj.ascii_only?
-              chunks << [ 'X', obj.size ].pack('an') << obj
-            else
-              chunks << [ 'X', obj.size, obj.unpack('U*') ].flatten.pack('anU*')
-            end
-            chunks.join # xml
-          end
         when 'B', 'b' # declare string as binary
-          if obj.size > CHUNK_SIZE
-            chunk = obj.slice!(0, CHUNK_SIZE)
-            chunks << [ 'b', CHUNK_SIZE ].pack('an') << chunk
-            write_object(TypeWrapper.new('B', obj), refs, chunks)
-          else
-            chunks << [ 'B', obj.bytesize ].pack('an') << obj
-            chunks.join # binary
-          end
+          write_binary(val)
         else  # type for list, map
-          write_object(obj, refs, chunks, hessian_type)
+          write_val(obj, refs, hessian_type)
         end
       when TrueClass  # 4.2.  boolean
         'T'           # true
@@ -74,36 +52,36 @@ module Hessian2
         'F'           # false
       when Time 
         if val.sec == 0
-          ["\x4b", val.to_i / 10 ].pack('al>') # 4.3.1.  Compact: date in minutes
+          [ BC_DATE_MINUTE, val.to_i / 10 ].pack('al>') # 4.3.1.  Compact: date in minutes
         else
-          [ "\x4a", val.to_i * 1000 + val.usec / 1000 ].pack('aQ>') # 4.3.  date
-        end 
+          [ BC_DATE, val.to_i * 1000 + val.usec / 1000 ].pack('aQ>') # 4.3.  date
+        end
       when Float                      # 4.4.  double
         case val
         when 0                        # 4.4.1.  Compact: double zero
-          "\x5b"  
+          BC_DOUBLE_ZERO  
         when 1                        # 4.4.2.  Compact: double one
-          "\x5c"  
+          BC_DOUBLE_ONE  
         when -128..127                # 4.4.3.  Compact: double octet
-          [ 'D', val ].pack('ac') 
+          [ BC_DOUBLE, val ].pack('ac') 
         when -32768..32767            # 4.4.4.  Compact: double short
-          [ 'D', val ].pack('as>')  
+          [ BC_DOUBLE, val ].pack('as>')  
         when -2147483648..2147483647  # 4.4.5.  Compact: double float
-          [ 'D', val ].pack('al>')  
+          [ BC_DOUBLE, val ].pack('al>')  
         else
-          [ 'D', val ].pack('aG') 
+          [ BC_DOUBLE, val ].pack('aG') 
         end
       when Fixnum             # 4.5.  int
         case val
-        when -16..47          # 4.5.1.  Compact: single octet integers
-          [ 'I', val ].pack('ac')
-        when -2048..2047      # 4.5.2.  Compact: two octet integers
-          [ 'I', val ].pack('as>')
+        when INT_DIRECT_MIN..INT_DIRECT_MAX         # 4.5.1.  Compact: single octet integers
+          [ val + BC_INT_ZERO ].pack('c')
+        when INT_BYTE_MIN..INT_BYTE_MAX      # 4.5.2.  Compact: two octet integers
+          [ BC_INT, val ].pack('as>')
         when -262144..262143  # 4.5.3.  Compact: three octet integers
           b2, b2r = val / 65536, val % 65536
-          [ 'I', b2, b2r ].pack('acs>')
+          [ BC_INT, b2, b2r ].pack('acs>')
         else
-          [ 'I', val ].pack('al>')  
+          [ BC_INT, val ].pack('al>')  
         end
       when Array  # TODO 4.6.  list
         idx = refs[val.object_id]
@@ -119,16 +97,16 @@ module Hessian2
       when Bignum   # 4.7.  long
         case val
         when -8..15 # 4.7.1.  Compact: single octet longs
-          [ 'L', val ].pack('ac')
+          [ BC_LONG, val ].pack('ac')
         when -2048..2047  # 4.7.2.  Compact: two octet longs
-          [ 'L', val ].pack('as>')
+          [ BC_LONG, val ].pack('as>')
         when -262144..262143  # 4.7.3.  Compact: three octet longs
           b2, b2r = val / 65536, val % 65536
-          [ 'L', b2, b2r ].pack('acs>')
+          [ BC_LONG, b2, b2r ].pack('acs>')
         when -4294967296..4294967295  # 4.7.4.  Compact: four octet longs
-          [ 'L', val ].pack('al>')
+          [ BC_LONG, val ].pack('al>')
         else
-          [ 'L', val ].pack('aq>')  # long
+          [ BC_LONG, val ].pack('aq>')  # long
         end
       when Hash   # TODO 4.8.  map
         idx = refs[val.object_id]
@@ -139,45 +117,17 @@ module Hessian2
         str = 'M'
         str << 't' << [ type.size, type ].pack('na*') if type
         val.each do |k, v|
-          str << write_object(k, refs)
-          str << write_object(v, refs)
+          str << write_val(k, refs)
+          str << write_val(v, refs)
         end
         str << 'z'
       when NilClass # 4.9.  null
-        'N'
+        BC_NULL
       when String   # 4.12.  string
-        if val.size < 32
-          val.ascii_only? ? [ val.size ].pack('n') << val : [ val.size, val.unpack('U*') ].flatten.pack('nU*')
-        elsif val.size < CHUNK_SIZE
-        else
-
-        if val.size > CHUNK_SIZE
-          chunk = val.slice!(0, CHUNK_SIZE)
-          if chunk.ascii_only?
-            chunks << [ 's', CHUNK_SIZE ].pack('an') << chunk
-          else
-            # unpack-pack if chunk incompatible with ASCII-8BIT
-            chunks << [ 's', CHUNK_SIZE, chunk.unpack('U*') ].flatten.pack('anU*')
-          end
-          write_object(val, refs, chunks)
-        else
-          if val.ascii_only?
-            chunks << [ 'S', val.size ].pack('an') << val
-          else
-            chunks << [ 'S', val.size, val.unpack('U*') ].flatten.pack('anU*')
-          end
-          chunks.join # string
-        end
+        write_string(val)
       when Symbol
-        str = val.to_s
-        [ 'S', str.size ].pack('an') << str # string
-      
-      
+        write_string(val.to_s)
       else  # 4.10.  object
-        h = {}.tap do |h| 
-          val.instance_variables.each {|var| h[var.to_s.delete("@")] = val.instance_variable_get(var) }
-        end
-
         unless type 
           # map ruby module to java package
           arr = val.class.to_s.split('::')
@@ -189,8 +139,57 @@ module Hessian2
           end
         end
 
-        write_object(h, refs, chunks, type)
+        h = {}.tap do |h|
+          val.instance_variables.each { |var| h[var.to_s.delete("@")] = val.instance_variable_get(var) }
+        end
+
+        str = [ BC_OBJECT_DEF, type.size ].pack('aC') << type
+        str << [ h.keys.size ].pack()
+        
+
+        write_val(h, refs, type)
       end
+    end 
+
+    def write_string(val, chunks = [])
+      while val.size > 0x8000 # 32768
+        chunk = val.slice!(0, 0x8000)
+        if chunk.ascii_only?
+          chunks << [ BC_STRING_CHUNK, 0x8000 ].pack('an') << chunk
+        else
+          # unpack-pack if chunk incompatible with ASCII-8BIT
+          chunks << [ BC_STRING_CHUNK, 0x8000, chunk.unpack('U*') ].flatten.pack('anU*')
+        end
+        write_string(val, chunks)
+      end
+
+      if val.size < 32
+        if val.ascii_only? 
+          chunks << [ val.size ].pack('C') << val 
+        else 
+          chunks << [ val.size, val.unpack('U*') ].flatten.pack('CU*')
+        end
+      else
+        if val.ascii_only?
+          chunks << [ BC_STRING, val.size ].pack('an') << val
+        else
+          chunks << [ BC_STRING, val.size, val.unpack('U*') ].flatten.pack('anU*')
+        end
+      end
+
+      chunks.join
+    end
+
+    def write_binary(val, chunks = [])
+      if val.size > 0x8000
+        chunk = val.slice!(0, 0x8000)
+        chunks << [ 'b', 0x8000 ].pack('an') << chunk
+        write_binary(val, chunks)
+      else
+        chunks << [ 'B', val.bytesize ].pack('an') << val
+      end
+
+      chunks.join
     end
 
   end
