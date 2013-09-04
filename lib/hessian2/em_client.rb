a3ed2f13b1d4ff0b23850e83008882c0450bbb50
takafan <takafan@163.com>
2013-09-04 18:25:50 +0800

    a

---
+++ lib/hessian2/em_client.rb
@@ -0,0 +1,51 @@
+require 'uri'
+require 'em-synchrony'
+require 'em-synchrony/em-http'
+require 'hessian2'
+
+module Hessian2
+  class EmClient
+    attr_accessor :user, :password
+    attr_reader :scheme, :host, :port, :path, :proxy
+
+    def initialize(url, proxy = {})
+      uri = URI.parse(url)
+      @scheme, @host, @port, @path = uri.scheme, uri.host, uri.port, uri.path
+      raise "Unsupported Hessian protocol: #{@scheme}" unless @scheme == 'http'
+      @proxy = proxy
+    end
+
+
+    def method_missing(id, *args)
+      return invoke(id.id2name, args)
+    end
+
+
+    private
+
+    def invoke(method, args)
+      result = nil
+      block = lambda do
+        EM::Synchrony.sync(
+          EM::HttpRequest.new("http://#{@host}:#{@port}", :connect_timeout => 5, :inactivity_timeout => 60).post(
+            path: @path,
+            head: {'Content-Type' => 'application/binary; charset=utf-8'},
+            body: Hessian2.call(method, args)
+          )
+        )
+      end
+
+      unless EM.reactor_running?
+        EM.synchrony do
+          result = Hessian2.parse_rpc(block.call.response)
+          EM.stop
+        end
+      else
+        result = Hessian2.parse_rpc(block.call.response)
+      end
+
+      result
+    end
+
+  end
+end
